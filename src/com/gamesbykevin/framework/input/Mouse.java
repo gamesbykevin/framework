a0cf69975096996e3b590e61aaace4b1442d5c0b
GOD <GOD@192.168.0.109>
2015-06-22 15:13:25 -0500

    Corrected "setMouseReleased" method to flag mouse drag false.
    The reason being you can't drag the mouse if the mouse has been released
    
    added additional comments

--- src/com/gamesbykevin/framework/input/Mouse.java
+++ src/com/gamesbykevin/framework/input/Mouse.java
@@ -22,17 +22,29 @@ public class Mouse
         location = null;
     }
     
+    /**
+     * Flag the mouse as moved
+     * @param location The (x,y) location of the mouse
+     */
     public void setMouseMoved(final Point location)
     {
         this.mouseMoved = true;
         setLocation(location);
     }
     
+    /**
+     * Has the mouse been moved?
+     * @return true=yes, false=no
+     */
     public boolean hasMouseMoved()
     {
         return this.mouseMoved;
     }
     
+    /**
+     * Flag the mouse as pressed
+     * @param e Mouse Event
+     */
     public void setMousePressed(MouseEvent e)
     {
         this.button = e.getButton();
@@ -40,28 +52,48 @@ public class Mouse
         setLocation(location);
     }
     
+    /**
+     * Has the mouse been pressed?
+     * @return true=yes, false=no
+     */
     public boolean isMousePressed()
     {
         return this.mousePressed;
     }
     
+    /**
+     * Flag the mouse as dragged
+     * @param location The (x,y) location of the mouse
+     */
     public void setMouseDragged(final Point location)
     {
         this.mouseDragged = true;
         setLocation(location);
     }
     
+    /**
+     * Has the mouse been dragged?
+     * @return true=yes, false=no
+     */
     public boolean isMouseDragged()
     {
         return this.mouseDragged;
     }
     
+    /**
+     * Flag the mouse as exited
+     * @param location The (x,y) location of the mouse
+     */
     public void setMouseExited(final Point location)
     {
         this.mouseExited = true;
         setLocation(location);
     }
     
+    /**
+     * Has the mouse exited?
+     * @return true=yes, false=no
+     */
     public boolean hasMouseExited()
     {
         return this.mouseExited;
@@ -73,23 +105,45 @@ public class Mouse
         setLocation(location);
     }
     
+    /**
+     * Has the mouse entered?
+     * @return true=yes, false=no
+     */
     public boolean hasMouseEntered()
     {
         return this.mouseEntered;
     }
     
+    /**
+     * Flag the mouse as released.<br>
+     * This will also flag drag false, since you can't drag if the mouse is released
+     * @param e Mouse Event
+     */
     public void setMouseReleased(MouseEvent e)
     {
         this.button = e.getButton();
         this.mouseReleased = true;
+        
+        //if the mouse was released then we can't be dragging it
+        this.mouseDragged = false;
+        
+        //update location
         setLocation(e.getPoint());
     }
     
+    /**
+     * Has the mouse been released?
+     * @return true=yes, false=no
+     */
     public boolean isMouseReleased()
     {
         return this.mouseReleased;
     }
     
+    /**
+     * Flag the mouse as clicked
+     * @param e Mouse Event
+     */
     public void setMouseClicked(MouseEvent e)
     {
         this.button = e.getButton();
@@ -97,6 +151,10 @@ public class Mouse
         setLocation(e.getPoint());
     }
     
+    /**
+     * Has the mouse been clicked?
+     * @return true=yes, false=no
+     */
     public boolean isMouseClicked()
     {
         return this.mouseClicked;
@@ -111,31 +169,55 @@ public class Mouse
         return this.location;
     }
     
+    /**
+     * Set the location of the mouse.
+     * @param location The (x,y) location
+     */
     private void setLocation(final Point location)
     {
         this.location = location;
     }
     
+    /**
+     * Get the button.
+     * @return The value of the button pressed
+     */
     public int getButton()
     {
         return this.button;
     }
     
+    /**
+     * Did we hit the left button?
+     * @return true if the left mouse button was pressed, false otherwise
+     */
     public boolean hitLeftButton()
     {
-        return (this.button == MouseEvent.BUTTON1);
+        return (getButton() == MouseEvent.BUTTON1);
     }
     
+    /**
+     * Did we hit the middle button?
+     * @return true if the middle mouse button was pressed, false otherwise
+     */
     public boolean hitMiddleButton()
     {
-        return (this.button == MouseEvent.BUTTON2);
+        return (getButton() == MouseEvent.BUTTON2);
     }
     
-     public boolean hitRightButton()
+    /**
+     * Did we hit the right button?
+     * @return true if the right mouse button was pressed, false otherwise
+     */
+    public boolean hitRightButton()
     {
-        return (this.button == MouseEvent.BUTTON3);
+        return (getButton() == MouseEvent.BUTTON3);
     }
-   
+    
+    /**
+     * Reset all mouse events.<br>
+     * We will not reset the mouse (x,y) location.
+     */
     public void reset()
     {
         this.mouseClicked  = false;
